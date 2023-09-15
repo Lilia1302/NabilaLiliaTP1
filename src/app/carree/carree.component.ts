345e50ba9dba9ae453f1ef9957d692ef4b30c163
Lilia Bouroubi <lilia.bouroubi@gmail.com>
2023-09-15 23:23:42 +0200

    final version

--- src/app/carree/carree.component.ts
+++ src/app/carree/carree.component.ts
@@ -6,8 +6,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrls: ['./carree.component.scss']
 })
 export class CarreeComponent {
- @Output() event = new EventEmitter<string>;
- public addEvent (color: string) {
-   this.event.emit(color);
- }
+  @Output() event = new EventEmitter<string>;
+
+  public addEvent(color: string) {
+    this.event.emit(color);
+  }
 }
